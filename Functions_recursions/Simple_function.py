1834535be406a81681c491359fd448da2cdf229d
Sachin Borude <sachinborude0610@gmail.com>
2026-02-03 22:48:29 +0530

    Added

---
+++ Functions_recursions/Simple_function.py
@@ -0,0 +1,13 @@
+def avg():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number 2: "))
+    num3 = int(input("Enter a number 3: "))
+    
+    avg = (num1+num2+num3)/3
+    print(avg)
+    
+avg()
+avg()
+avg()
+    
+    
