96fae2e1d956706dc4791849995541c0fa74d146
Sachin Borude <sachinborude0610@gmail.com>
2026-02-05 07:23:33 +0530

    Added

--- Functions_recursions/1_program.py
+++ Functions_recursions/1_program.py
@@ -1,11 +1,18 @@
 ''''def fun1():
     print("Hello")
     
-fun1()'''
+fun1()
 
 
 #WAP to greet a user with "Good day" using functions.
 def fun2():
     print("Good Day")
     
-fun2()
+fun2()'''
+
+def greet(name):
+    gr = "Hello "+ name
+    return gr
+
+a = greet("Sachin ")
+print(a)
--- Functions_recursions/Arguments_fn.py
+++ Functions_recursions/Arguments_fn.py
@@ -1,6 +1,6 @@
-def Good_day(name, ending):
+def Good_day(name, ending):# name , ending are parameter
     print("Good Day, "+ name, ending)
     
-Good_day("Sachin","Thank you")
+Good_day("Sachin","Thank you") # inside the function this is called arguments
 Good_day("Nandini", "Thank you")
 Good_day("Bhushan", "Thank yoy")
