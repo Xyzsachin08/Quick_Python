91a17dd4a86b2f1eef928164ff3518a73c9b27d2
Sachin Borude <sachinborude0610@gmail.com>
2026-02-05 07:41:40 +0530

    Added

--- Functions_recursions/Defulat_Arguments.py
+++ Functions_recursions/Defulat_Arguments.py
@@ -1,4 +1,13 @@
 def goodday(name, ending="Thank you"):
     print(f"Good Day, {name}")
+    print(ending)
     
 goodday("Sachin")
+
+
+
+def greet(name="nisanchu"):
+    print(name)
+    
+greet()
+greet("sachin")
