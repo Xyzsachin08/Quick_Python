82e036092d4ab94480a4bcc9c2f5c22b9e698f45
Sachin Borude <sachinborude0610@gmail.com>
2026-02-07 15:21:24 +0530

    Added

---
+++ Functions_recursions/Practise_set_8/05_problem.py
@@ -0,0 +1,15 @@
+# WAP pattern 
+'''
+***
+**
+*   '''
+
+
+def pattern(n):
+    if(n == 0):
+        return
+    print("*"*n)
+    pattern (n-1)
+    
+n = int(input("enter a number "))
+print(pattern(n))
