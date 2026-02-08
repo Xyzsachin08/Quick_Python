8f40060a153fb707d523d99ab5af9136c28f81fb
Sachin Borude <sachinborude0610@gmail.com>
2026-02-08 11:54:21 +0530

    Added

---
+++ File_Input&Output/01_file.py
@@ -0,0 +1,14 @@
+'''
+a = "A very long string with emails"
+
+
+
+emails = []
+3 second 
+'''
+f = open("file.txt", "r") #read the file
+
+data = f.read() #read the data
+print(data) #display the data
+f.close()
+
