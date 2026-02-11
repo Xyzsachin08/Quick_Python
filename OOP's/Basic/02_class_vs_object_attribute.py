583d070c59b22086bfc685f01cd37a9cb7ec6483
Sachin Borude <sachinborude0610@gmail.com>
2026-02-11 10:04:07 +0530

    Practice: Python class and object attributes  Learning OOP concepts in Python

---
+++ OOP's/Basic/02_class_vs_object_attribute.py
@@ -0,0 +1,11 @@
+class Employee:
+    language = "python" #This is a class aatribute
+    salary = 600000
+    
+sachin = Employee()
+sachin.name = "borude"  #This is a object aatribute
+print(sachin.name, sachin.language, sachin.salary)
+
+bhushan = Employee()
+bhushan.name = "tarte"
+print(bhushan.name, bhushan.language, bhushan.salary)
