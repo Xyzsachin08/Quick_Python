caf480ee614eec4df45bb2e233a818fd746830ff
Sachin Borude <sachinborude0610@gmail.com>
2026-02-11 20:31:03 +0530

    Practice:  declare in function self   Learning OOP concepts in Python

---
+++ OOP's/Self_Parameter/01_self_argument.py
@@ -0,0 +1,11 @@
+class Student:
+    age = 85
+    std = 96
+    roll_no = 45
+    clg_name = "trinity"
+    
+    def student(self):
+        print(f"The age of student is {self.age} and roll no is {self.roll_no} and clg name is{self.clg_name} the standard is {self.std}")
+        
+s1 =  Student()
+print(s1)
