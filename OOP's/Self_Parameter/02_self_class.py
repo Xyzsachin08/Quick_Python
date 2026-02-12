091c8789f13d8f30640f588e6daca756bc5d5b62
Sachin Borude <sachinborude0610@gmail.com>
2026-02-12 11:41:36 +0530

    practise class & function

---
+++ OOP's/Self_Parameter/02_self_class.py
@@ -0,0 +1,15 @@
+class Employee:
+    language = "python"
+    salary = 600000
+    
+sachin = Employee()
+sachin.name = "borude"
+print(sachin.name, sachin.language, sachin.salary)
+
+bhushan = Employee()
+bhushan.name = "tarte"
+print(bhushan.name, bhushan.language, bhushan.salary)
+
+
+#here name is a class attribute and lanaguage and salary is a class attribute 
+#that are directly belongs to the class
