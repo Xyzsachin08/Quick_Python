6d671e33cd47be3bc85b4cef3505bb48aaf2bbef
Sachin Borude <sachinborude0610@gmail.com>
2026-02-15 16:13:48 +0530

    Updated

--- OOP's/oop_practise_set/02_problem.py
+++ OOP's/oop_practise_set/02_problem.py
@@ -1,2 +1,19 @@
 #Write a class "calculator" capable of finding square , cube and square root of a number.
 
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+        
+    def square(self):
+        print(f"The square is{self.n*self.n}")
+        
+    def cube(self):
+        print(f"The cube is{self.n*self.n*self.n}")
+        
+    def squareroot(self):
+        print(f"The square root is{self.n**1/2}")
+        
+c1 = Calculator(4)
+c1.square()
+c1.squareroot()
+c1.cube()
