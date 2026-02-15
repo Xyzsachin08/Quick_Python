78cbcd4ec8186969511185e76a3df28d58f7cdfe
Sachin Borude <sachinborude0610@gmail.com>
2026-02-15 15:45:56 +0530

    problem 1 in practise set

---
+++ OOP's/oop_practise_set/01_problem.py
@@ -0,0 +1,16 @@
+class Programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+        
+h1 = Programmer("Harry", 12345, 12345)
+print(h1.name, h1.salary, h1.pin, h1.company)
+
+s1 = Programmer("Sachin", 75000, 413701)
+print(s1.company, s1.name,s1.salary, s1.pin)
+
+n1 = Programmer("Nisanchu", 70000, 413701)
+print(n1.pin, n1.company, n1.salary, n1.name)
+        
